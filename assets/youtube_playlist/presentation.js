406d0e00144de465bc5b583e595a1f992335d8b1
Travis Tidwell <travist349@gmail.com>
2012-06-20 22:38:21 -0500

    Adding the presentation js file.

---
+++ assets/youtube_playlist/presentation.js
@@ -0,0 +1,74 @@
+
+
+
+
+/**************************************
+ *
+ * Building jQuery Widgets
+ *
+ *************************************/
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+
+
+
+
+
+
+
+
+/**************************************
+ *
+ * ...but really the title should be...
+ *
+ * Building Object Oriented, Services
+ * driven, jQuery Mobile widgets...
+ *
+ *************************************/
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+/**************************************
+ *
+ * Code and Demo:
+ *
+ * https://github.com/travist/youtube_playlist
+ * http://travistidwell.com/youtube_playlist
+ *
+ *************************************/
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**************************************
+ *
+ * "...it's business time!"
+ *
+ *************************************/
+
+ 
