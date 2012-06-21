192ebd05b98ace95ddd7255fcf2368a0e0a5fccc
Travis Tidwell <travist349@gmail.com>
2012-06-20 21:45:09 -0500

    changed the get to init.

--- youtube_playlist.js
+++ youtube_playlist.js
@@ -2,7 +2,7 @@
 jQuery.fn.youtube_playlist = function(options) {
   return jQuery(this).each(function() {
     if ('youtube_playlist' in this) {
-      this.youtube_playlist.set(options);
+      this.youtube_playlist.init(options);
     }
     else {
       this.youtube_playlist = new youtube.playlist(jQuery(this), options);
