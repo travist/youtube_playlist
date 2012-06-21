fdd37f0166af795709c65c840ee82c842efb7857
Travis Tidwell <travist349@gmail.com>
2012-06-21 11:51:20 -0500

    Fixd how the display is populated.

--- assets/youtube_playlist/src/youtube.teaser.js
+++ assets/youtube_playlist/src/youtube.teaser.js
@@ -19,11 +19,14 @@ youtube.teaser.prototype.constructor = youtube.teaser;
 /** Override the create method. */
 youtube.teaser.prototype.create = function(context) {
   youtube.display.prototype.create.call(this, context);
-  var display = jQuery('<li><a href="' + this.options.link + '">\
-    <img src="' + this.options.thumbnail + '" />\
-    <h3>' + this.options.title + '</h3>\
-    <p>' + this.options.description + '</p>\
-  </a></li>');
+  var display = jQuery(document.createElement('li'));
+  this.link = jQuery(document.createElement('a'));
+  this.title = jQuery(document.createElement('h3'));
+  this.thumbnail = jQuery(document.createElement('img'));
+  this.description = jQuery(document.createElement('p'));
+
+  // Build the display.
+  display.append(this.link.append(this.thumbnail).append(this.title).append(this.description));
   context.append(display);
   return display;
 };
@@ -31,4 +34,8 @@ youtube.teaser.prototype.create = function(context) {
 /** Override the init method. */
 youtube.teaser.prototype.init = function(options) {
   youtube.display.prototype.init.call(this, options);
+  this.link.attr({href: options.link});
+  this.title.text(options.title);
+  this.thumbnail.attr({src: options.thumbnail});
+  this.description.text(options.description);
 };
